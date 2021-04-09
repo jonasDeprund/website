552857d742c50e028fe94e8d171b9ab33c71fefb
Jonas Deprund <jonasdeprund@gmail.com>
2021-04-09 21:26:47 +0200

    animation done

--- app.js
+++ app.js
@@ -42,16 +42,46 @@
 
 ////////
 ////
-//// Code
+//// SCROLL MAGIC
 ////
 ///////
 
-const controller = new ScrollMagic.Controller()
+// const controller = new ScrollMagic.Controller()
 
-const exploreScene = new ScrollMagic.Scene({
-  triggerElement: '.hike-exp',
-  triggerHook: 0.5,
-})
-  .addIndicators({ colorStart: 'white', colorTrigger: 'white' })
-  .setClassToggle('.hike-exp', 'active')
-  .addTo(controller)
+// const exploreScene = new ScrollMagic.Scene({
+//   triggerElement: '.hike-exp',
+//   triggerHook: 0.5,
+// })
+//   .addIndicators({ colorStart: 'white', colorTrigger: 'white' })
+//   .setClassToggle('.hike-exp', 'active')
+//   .addTo(controller)
+
+////////
+////
+//// GSAP
+////
+///////
+
+let controller
+let slideScene
+
+function animateSlides() {
+  controller = new ScrollMagic.Controller()
+  const sliders = document.querySelectorAll('.slide')
+  const nav = document.querySelector('.nav-header')
+  sliders.forEach((slide) => {
+    const revealImg = slide.querySelector('.reveal-img')
+    const img = slide.querySelector('img')
+    const revealText = slide.querySelector('.reveal-text')
+    // GSAP
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: 'power2.inOut' },
+    })
+    slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' })
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '-=1')
+    slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=0.75')
+    slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.5')
+  })
+}
+
+animateSlides()
