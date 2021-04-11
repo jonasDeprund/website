ec92e5df9499426d62783cbb83a40744861219a5
Jonas Deprund <jonasdeprund@gmail.com>
2021-04-11 18:15:29 +0200

    gsap animation scroll done

--- app.js
+++ app.js
@@ -64,12 +64,14 @@
 
 let controller
 let slideScene
+let pageScene
 
 function animateSlides() {
   controller = new ScrollMagic.Controller()
   const sliders = document.querySelectorAll('.slide')
   const nav = document.querySelector('.nav-header')
-  sliders.forEach((slide) => {
+  // loop over each slide
+  sliders.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector('.reveal-img')
     const img = slide.querySelector('img')
     const revealText = slide.querySelector('.reveal-text')
@@ -81,6 +83,39 @@ function animateSlides() {
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '-=1')
     slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=0.75')
     slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.5')
+    // Scene
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      .addIndicators({
+        colorStart: 'white',
+        colorTrigger: 'white',
+        name: 'slide',
+      })
+      .addTo(controller)
+    // New animation
+    const pageTl = gsap.timeline()
+    let nextSlide = slide.length - 1 === index ? 'end' : slides[index + 1]
+    pageTl.fromTo(nextSlide, { y: '0%' }, { y: '100%' })
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0 })
+    pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' }, '-= 0.5')
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: '100%',
+      triggerHook: 0,
+    })
+      .addIndicators({
+        colorStart: 'white',
+        colorTrigger: 'white',
+        name: 'page',
+        indent: 200,
+      })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTl)
+      .addTo(controller)
   })
 }
 
